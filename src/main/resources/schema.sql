82f91411170e5b5d4f7704e5e0e2f76fad06c4b9
Jack Wang <3439933260@qq.com>
2026-07-13 14:01:34 +0800

    驼峰下划线命名自动转换

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,3 +11,19 @@ CREATE TABLE IF NOT EXISTS asset_snapshot (
     amount NUMERIC NOT NULL,
     FOREIGN KEY (category_id) REFERENCES asset_category(id)
 );
+
+INSERT INTO asset_category (name, created_date)
+SELECT '股票', CURRENT_DATE
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM asset_category
+    WHERE name = '股票'
+);
+
+INSERT INTO asset_category (name, created_date)
+SELECT '现金', CURRENT_DATE
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM asset_category
+    WHERE name = '现金'
+);
--- src/test/java/org/jack/wealthflow/WealthFlowApplicationTests.java
+++ src/test/java/org/jack/wealthflow/WealthFlowApplicationTests.java
@@ -26,4 +26,5 @@ class WealthFlowApplicationTests {
 
         assertNotNull(categories);
     }
+
 }
