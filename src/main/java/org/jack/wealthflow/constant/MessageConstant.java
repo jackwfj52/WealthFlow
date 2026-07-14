f7cb3caf54c7625f646f0799d03621d542388656
Jack Wang <3439933260@qq.com>
2026-07-15 02:05:34 +0800

    AssetCategoryMapper实现

---
+++ src/main/java/org/jack/wealthflow/constant/MessageConstant.java
@@ -0,0 +1,16 @@
+package org.jack.wealthflow.constant;
+
+public class MessageConstant {
+
+
+    public static final String ASSET_CATEGORY_ADD_FAILED = "资产分类新增失败";
+    public static final String ID_NOT_EMPTY = "ID不能为空";
+    public static final String NAME_NOT_EMPTY = "名称不能为空";
+    public static final String NAME_NOT_TOO_LONG = "名称不能超过50个字符";
+    public static final String NAME_ALREADY_EXISTS = "名称已存在";
+    public static final String ASSET_CATEGORY_NOT_FOUND = "该资产分类不存在";
+    public static final String ASSET_CATEGORY_UPDATE_FAILED = "资产分类修改失败";
+    public static final String ASSET_CATEGORY_DELETE_FAILED = "资产分类删除失败";
+    public static final String ASSET_CATEGORY_HAS_SNAPSHOTS = "资产分类下存在快照，无法删除";
+
+}
