39ca363d6affba891cc117425573e60c7058932a
Jack Wang <3439933260@qq.com>
2026-07-28 18:25:36 +0800

    快照错误提示

--- src/main/java/org/jack/wealthflow/constant/MessageConstant.java
+++ src/main/java/org/jack/wealthflow/constant/MessageConstant.java
@@ -13,4 +13,16 @@ public class MessageConstant {
     public static final String ASSET_CATEGORY_DELETE_FAILED = "资产分类删除失败";
     public static final String ASSET_CATEGORY_HAS_SNAPSHOTS = "资产分类下存在快照，无法删除";
 
+    public static final String SNAPSHOT_NOT_FOUND = "该资产快照不存在";
+    public static final String SNAPSHOT_DATE_EXISTS = "该日期已经存在快照，请使用编辑功能";
+    public static final String SNAPSHOT_DATE_NOT_EMPTY = "快照日期不能为空";
+    public static final String SNAPSHOT_DATE_CANNOT_BE_FUTURE = "快照日期不能晚于今天";
+    public static final String SNAPSHOT_ITEMS_NOT_EMPTY = "快照明细不能为空";
+    public static final String SNAPSHOT_CATEGORY_ID_NOT_EMPTY = "快照明细的资产分类ID不能为空";
+    public static final String SNAPSHOT_AMOUNT_INVALID = "快照金额必须大于0";
+    public static final String SNAPSHOT_CATEGORY_DUPLICATE = "同一快照中不能包含重复的资产分类";
+    public static final String ASSET_SNAPSHOT_ADD_FAILED = "资产快照新增失败";
+    public static final String ASSET_SNAPSHOT_UPDATE_FAILED = "资产快照修改失败";
+    public static final String ASSET_SNAPSHOT_DELETE_FAILED = "资产快照删除失败";
+
 }
--- src/main/java/org/jack/wealthflow/exception/ErrorCode.java
+++ src/main/java/org/jack/wealthflow/exception/ErrorCode.java
@@ -4,8 +4,10 @@ public enum ErrorCode {
 
     PARAM_INVALID(40001, 400, "请求参数不合法"),
     CATEGORY_NOT_FOUND(40401, 404, "该资产分类不存在"),
+    SNAPSHOT_NOT_FOUND(40402, 404, "该资产快照不存在"),
     CATEGORY_NAME_EXISTS(40901, 409, "名称已存在"),
     CATEGORY_HAS_SNAPSHOTS(40902, 409, "资产分类下存在快照，无法删除"),
+    SNAPSHOT_DATE_EXISTS(40903, 409, "该日期已经存在快照，请使用编辑功能"),
     SERVER_ERROR(50000, 500, "服务器内部错误");
 
     private final int code;
